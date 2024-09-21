b2acf5ac75aacdb5000717d02d75e0db12ed301a
sadmanryanrad <sadmanryanriad@gmail.com>
2024-09-21 19:54:06 +0600

    new subdomain added

--- index.js
+++ index.js
@@ -1,107 +1,105 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000
-require('dotenv').config()
-var nodemailer = require('nodemailer');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+var nodemailer = require("nodemailer");
 
 //middleware
-app.use(cors(
-    {
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://shabuj-global-reg.web.app",
+      "https://registration.studyuk.today",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-        origin: ['http://localhost:5173',
-            'http://localhost:5174',
-            'https://shabuj-global-reg.web.app'
-        ],
-        credentials: true
-
-    }
-))
-app.use(express.json())
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gx7mkcg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
-
 
 const registrations = client.db("ICEF").collection("registrations");
 
 const dbConnect = async () => {
-    try {
-        await client.connect();
+  try {
+    await client.connect();
 
-        app.get('/', async (req, res) => {
-            res.send('Server is running')
-        })
+    app.get("/", async (req, res) => {
+      res.send("Server is running");
+    });
 
-        app.get('/registrations', async (req, res) => {
-            const result = await registrations.find().toArray()
-            res.send(result)
-        })
-        app.get("/registration/:id", async (req, res) => {
-            const id = req.params.id
-            const filter = { _id: new ObjectId(id) }
-            const studentData = registrations.find(filter)
-            const result = await studentData.toArray()
-            res.send(result)
-        })
+    app.get("/registrations", async (req, res) => {
+      const result = await registrations.find().toArray();
+      res.send(result);
+    });
+    app.get("/registration/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const studentData = registrations.find(filter);
+      const result = await studentData.toArray();
+      res.send(result);
+    });
 
-        app.get('/counsellors/:email',  async (req, res) => {
-            const query = { cpMail: req.params?.email }
-            const result = await registrations.find(query).toArray()
-            res.send(result)
+    app.get("/counsellors/:email", async (req, res) => {
+      const query = { cpMail: req.params?.email };
+      const result = await registrations.find(query).toArray();
+      res.send(result);
+    });
 
-        })
+    app.post(`/registrations`, async (req, res) => {
+      const registration = req.body;
+      const result = await registrations.insertOne(registration);
+      res.send(result);
+    });
 
-        app.post(`/registrations`, async (req, res) => {
-            const registration = req.body
-            const result = await registrations.insertOne(registration)
-            res.send(result)
-        })
+    app.patch("/registrationPatchStatus/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          formData: req.body.formData,
+          cpMail: req.body.counsellorMail,
+          cpName: req.body.counsellorName,
+        },
+      };
+      const result = await registrations.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
-        app.patch('/registrationPatchStatus/:_id', async (req, res) => {
-            const id = req.params._id
-            const query = { _id: new ObjectId(id) }
-            const updateDoc = {
-                $set: {
-                    formData: req.body.formData,
-                    cpMail: req.body.counsellorMail,
-                    cpName: req.body.counsellorName
-                }
-            }
-            const result = await registrations.updateOne(query, updateDoc)
-            res.send(result)
-        })
+    //   nodemailer
+    app.post("/sendMail", async (req, res) => {
+      const mail = req.body.mail;
+      const name = req.body.name;
+      const to = req.body.to;
+      const subject = req.body.subject;
+      console.log(mail, to, subject);
+      var nodemailer = require("nodemailer");
 
-        //   nodemailer
-        app.post('/sendMail', async (req, res) => {
-            const mail = req.body.mail
-            const name = req.body.name
-            const to = req.body.to
-            const subject = req.body.subject
-            console.log(mail, to, subject)
-            var nodemailer = require('nodemailer');
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "shabujglobaleducation24@gmail.com",
+          pass: "qibl nicy hzea cdfd",
+        },
+      });
 
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'shabujglobaleducation24@gmail.com',
-                    pass: 'qibl nicy hzea cdfd'
-                }
-            });
-
-            var mailOptions = {
-                from: 'shabujglobaleducation24@gmail.com',
-                to: to,
-                subject: subject,
-                html: `<div>
+      var mailOptions = {
+        from: "shabujglobaleducation24@gmail.com",
+        to: to,
+        subject: subject,
+        html: `<div>
                         <div style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); overflow: hidden;">
                             <div style="background-color: #0073e6; color: #ffffff; padding: 20px; text-align: center;">
                                 <h1 style="margin: 0;">Shabuj Global Education</h1>
@@ -117,46 +115,46 @@ const dbConnect = async () => {
                     Satmosjid Road, Dhanmondi, Dhaka-1207</p>
                     </div>
     </div>
-            </div>`
-            };
+            </div>`,
+      };
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-        })
-        app.post('/sendMails', async (req, res) => {
-            const mail = req?.body?.mail
-            const firstName = mail.firstName
-            const lastName = mail.lastName
-            const mobileNo = mail.mobileNo
-            const email = mail.email
-            const academic = mail.academic
-            const country = mail.country
-            const university = mail.university
-            const course = mail.course
-            const name = req.body.name
-            const to = req.body.to
-            const subject = req.body.subject
-            console.log(mail, to, subject)
-            var nodemailer = require('nodemailer');
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    });
+    app.post("/sendMails", async (req, res) => {
+      const mail = req?.body?.mail;
+      const firstName = mail.firstName;
+      const lastName = mail.lastName;
+      const mobileNo = mail.mobileNo;
+      const email = mail.email;
+      const academic = mail.academic;
+      const country = mail.country;
+      const university = mail.university;
+      const course = mail.course;
+      const name = req.body.name;
+      const to = req.body.to;
+      const subject = req.body.subject;
+      console.log(mail, to, subject);
+      var nodemailer = require("nodemailer");
 
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'shabujglobaleducation24@gmail.com',
-                    pass: 'qibl nicy hzea cdfd'
-                }
-            });
+      var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "shabujglobaleducation24@gmail.com",
+          pass: "qibl nicy hzea cdfd",
+        },
+      });
 
-            var mailOptions = {
-                from: 'shabujglobaleducation24@gmail.com',
-                to: to,
-                subject: subject,
-                html: `
+      var mailOptions = {
+        from: "shabujglobaleducation24@gmail.com",
+        to: to,
+        subject: subject,
+        html: `
                 <div>
                     <div style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); overflow: hidden;">
                         <div style="background-color: #0073e6; color: #ffffff; padding: 20px; text-align: center;">
@@ -164,7 +162,9 @@ const dbConnect = async () => {
                         </div>
                         <div style="padding: 20px;">
                             <p style="font-size: 16px; color: #333333;">Hello, ${name}</p>
-                            <p style="font-size: 16px; color: #333333;">You have a meeting with ${firstName + " " + lastName}</p>
+                            <p style="font-size: 16px; color: #333333;">You have a meeting with ${
+                              firstName + " " + lastName
+                            }</p>
                             <p style="font-size: 16px; color: #333333;">Phone no: ${mobileNo}</p>
                             <p style="font-size: 16px; color: #333333;">Email: ${email}</p>
                             <p style="font-size: 16px; color: #333333;">Academic qualification: ${academic}</p>
@@ -179,31 +179,25 @@ const dbConnect = async () => {
                             Satmosjid Road, Dhanmondi, Dhaka-1207</p>
                         </div>
                     </div>
-                </div>`
+                </div>`,
+      };
 
-            };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error.name, error.message);
+  } finally {
+    console.log("Database Connected successfully ✅");
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-        })
-
-
-    } catch (error) {
-        console.log(error.name, error.message);
-    } finally {
-        console.log("Database Connected successfully ✅");
-
-        app.listen(port, () => {
-            console.log(`Your port is ${port}`)
-        })
-    }
-}
+    app.listen(port, () => {
+      console.log(`Your port is ${port}`);
+    });
+  }
+};
 dbConnect();
-
-
-
